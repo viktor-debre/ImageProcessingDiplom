76a338754c21d9759a17f40af827a6e15c4c4e3c
viktor <viktor.debre2002@gmail.com>
2023-02-09 16:41:27 +0200

    Change Manhattan dictance method

--- ImageProcessingDiplom/OpenCvServices/ManhattanDictanceProvider.cs
+++ ImageProcessingDiplom/OpenCvServices/ManhattanDictanceProvider.cs
@@ -9,21 +9,37 @@ namespace ImageProcessingDiplom.OpenCvServices
 
         public int CountThresholdMathes(int[,] distances)
         {
+            var minimums = FindMinimumsOfDistances(distances);
             int count = 0;
 
-            for (int i = 0; i < distances.GetLength(0); i++)
+            for (int i = 0; i < minimums.Length; i++)
             {
-                for (int j = 0; j < distances.GetLength(1); j++)
+                if (minimums[i] < BRISK_BYTE_COUNT * THRESHOLD_MATHES)
                 {
-                    if (distances[i, j] < BRISK_BYTE_COUNT * THRESHOLD_MATHES)
-                    {
-                        count++;
-                        break;
-                    }
+                    count++;
                 }
             }
 
             return count;
+        }
+
+        public int[] FindMinimumsOfDistances(int[,] distances)
+        {
+            int[] minimums = new int[distances.GetLength(0)];
+
+            for (int i = 0; i < distances.GetLength(0); i++)
+            {
+                minimums[i] = BRISK_BYTE_COUNT;
+                for (int j = 0; j < distances.GetLength(1); j++)
+                {
+                    if (distances[i, j] < minimums[i])
+                    {
+                        minimums[i] = distances[i, j];
+                    }
+                }
+            }
+
+            return minimums;
         }
     }
 }
