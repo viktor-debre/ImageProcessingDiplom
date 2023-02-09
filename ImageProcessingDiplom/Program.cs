a2369e2369240d41a359fb96af94ced7cdd77d18
viktor <viktor.debre2002@gmail.com>
2023-02-09 16:41:02 +0200

    Add static files

--- ImageProcessingDiplom/Program.cs
+++ ImageProcessingDiplom/Program.cs
@@ -4,6 +4,7 @@ using ImageProcessingDiplom.OpenCvServices;
 using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics;
 
+//Dependency Injection
 var services = new ServiceCollection();
 
 var serviceProvider = services.AddServices()
@@ -13,38 +14,48 @@ var _hamming = serviceProvider.GetService<IHammingProvider>();
 var _manhattan = serviceProvider.GetService<IManhattanDictanceProvider>();
 var _medoid = serviceProvider.GetService<MedoidFinder>();
 
+
+//Program
+string results = "";
+var projectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
 var imagePathes = new List<string>
 {
-    "D:\\repos\\diplom\\image1.png",
-    "D:\\repos\\diplom\\image2.png",
-    "D:\\repos\\diplom\\image3.png"
+   projectPath + "\\Images\\image1.png",
+   projectPath + "\\Images\\image2.png",
+   projectPath + "\\Images\\image3.png"
 };
 
 BriskDetector detector1 = new BriskDetector(imagePathes[0]);
 BriskDetector detector2 = new BriskDetector(imagePathes[1]);
 BriskDetector detector3 = new BriskDetector(imagePathes[2]);
 
+results += "Number of keypoints: " + detector1.Keypoints.Size + '\n';
 Console.WriteLine("Number of keypoints: " + detector1.Keypoints.Size);
+results += "Descriptors shape: " + detector1.Descriptors.Rows + " x " + detector1.Descriptors.Cols + '\n';
 Console.WriteLine("Descriptors shape: " + detector1.Descriptors.Rows + " x " + detector1.Descriptors.Cols);
+
+Stopwatch stopwatch = Stopwatch.StartNew();
 
 
 var distances1 = _hamming.FindHammingDistance(detector1.Descriptors, detector1.Descriptors);
 var distances2 = _hamming.FindHammingDistance(detector1.Descriptors, detector2.Descriptors);
 var distances3 = _hamming.FindHammingDistance(detector1.Descriptors, detector3.Descriptors);
 
-Stopwatch stopwatch = Stopwatch.StartNew();
 
 var mathes1 = _manhattan.CountThresholdMathes(distances1);
+results += "Mathes 1 with 1: " + mathes1 + '\n';
 Console.WriteLine("Mathes 1 with 1: " + mathes1);
 
 var mathes2 = _manhattan.CountThresholdMathes(distances2);
+results += "Mathes 1 with 2: " + mathes2 + '\n';
 Console.WriteLine("Mathes 1 with 2: " + mathes2);
 
 var mathes3 = _manhattan.CountThresholdMathes(distances3);
+results += "Mathes 1 with 3: " + mathes3 + '\n';
 Console.WriteLine("Mathes 1 with 3: " + mathes3);
 
 stopwatch.Stop();
-
+results += "distance array: " + distances1.Length + " time elapsed: " + stopwatch.ElapsedMilliseconds + '\n';
 Console.WriteLine("distance array: " + distances1.Length + " time elapsed: " + stopwatch.ElapsedMilliseconds);
 
 //Medoid search
@@ -52,9 +63,11 @@ var medoidIndex = _medoid.FindMedoidIndex(distances2);
 
 var descriptors = detector1.Descriptors;
 var descriptor = descriptors.Row(medoidIndex).GetRawData();
-Console.WriteLine("Medoid is descriptor number " + medoidIndex + " with value: ");
+results += "Medoid is descriptor number " + (medoidIndex + 1) + " with value: " + '\n';
+Console.WriteLine("Medoid is descriptor number " + (medoidIndex + 1) + " with value: ");
 foreach (var item in descriptor)
 {
+    results += item + " ";
     Console.Write(item + " ");
 }
 
@@ -63,15 +76,20 @@ var maxDstanceIndex = _medoid.FindIndexOfMaxDistance(distances2, medoidIndex);
 
 var closestDescriptorToMedoid = descriptors.Row(minDistanceIndex).GetRawData();
 var farthestDescriptorToMedoid = descriptors.Row(maxDstanceIndex).GetRawData();
-
-Console.WriteLine("\n\nClosest to medoid is descriptor number " + minDistanceIndex + " with value: ");
+results += "\n\nClosest to medoid is descriptor number " + (minDistanceIndex + 1) + " with value: " + '\n';
+Console.WriteLine("\n\nClosest to medoid is descriptor number " + (minDistanceIndex + 1) + " with value: ");
 foreach (var item in closestDescriptorToMedoid)
 {
+    results += item + " ";
     Console.Write(item + " ");
 }
 
-Console.WriteLine("\n\nFarthest to medoid is descriptor number " + maxDstanceIndex + " with value: ");
+results += "\n\nFarthest to medoid is descriptor number " + (maxDstanceIndex + 1) + " with value: " + '\n';
+Console.WriteLine("\n\nFarthest to medoid is descriptor number " + (maxDstanceIndex + 1) + " with value: ");
 foreach (var item in farthestDescriptorToMedoid)
 {
+    results += item + " ";
     Console.Write(item + " ");
 }
+
+File.WriteAllText(projectPath + "\\results.txt", results);
