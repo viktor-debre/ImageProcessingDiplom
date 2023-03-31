e5d459510a7179ec16dbaeea780f7cf70479281d
viktor <viktor.debre2002@gmail.com>
2023-03-31 13:33:07 +0300

    new update

--- ImageProcessingDiplom/OpenCvServices/ManhattanDictanceProvider.cs
+++ ImageProcessingDiplom/OpenCvServices/ManhattanDictanceProvider.cs
@@ -38,6 +38,102 @@
             return result;
         }
 
+        private int FindMinimumsDistanceToElementFrom(int index, int[,] distances2, int[,] distances3)
+        {
+            int min = BRISK_BYTE_COUNT;
+            for (int j = 0; j < distances2.GetLength(1); j++)
+            {
+                if (distances2[index, j] < min)
+                {
+                    min = distances2[index, j];
+                }
+                if (distances3[index, j] < min)
+                {
+                    min = distances3[index, j];
+                }
+            }
+
+            return min;
+        }
+
+
+        private int FindMinimumsDistanceToElementFromSameEtalon(int index, int[,] distances1)
+        {
+            int min = BRISK_BYTE_COUNT;
+            for (int j = 0; j < distances1.GetLength(1); j++)
+            {
+                if (j == index) continue;
+                if (distances1[index, j] < min)
+                {
+                    min = distances1[index, j];
+                }
+            }
+
+            return min;
+        }
+
+        public int[] FindMinimumV(int[,] distances1, int[,] distances2, int[,] distances3)
+        {
+            int[] minimums = new int[distances2.GetLength(0)];
+
+            for (int i = 0; i < distances2.GetLength(0); i++)
+            {
+                int pz_E = FindMinimumsDistanceToElementFrom(i, distances2, distances3);
+                int pz_S = FindMinimumsDistanceToElementFromSameEtalon(i, distances1);
+                minimums[i] = pz_E - pz_S;
+            }
+
+            return minimums;
+        }
+
+        public int[] GetTopIndices(int[] arr, int max)
+        {
+            // sort the array in descending order
+            int[] sortedArr = arr.OrderByDescending(x => x).ToArray();
+
+            // create an array to store the indices of the top values
+            int[] topIndices = new int[max];
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (sortedArr.Contains(arr[i]) && count < max)
+                {
+                    topIndices[count] = i;
+                    count++;
+                }
+            }
+
+            return topIndices;
+        }
+
+        public int[,] TakeTopElementsFromIndexes(int[,] arr, int[] indexes)
+        {
+            int[,] newArr = new int[indexes.Count(), indexes.Count()];
+            for (int i = 0; i < indexes.Count(); i++)
+            {
+                for (int j = 0; j < indexes.Count(); j++)
+                {
+                    newArr[i,j] = arr[indexes[i], indexes[j]];
+                }
+            }
+
+            return newArr;
+        }
+
+        public int[,] TakeTopElementsFrom2Indexes(int[,] arr, int[] indexes1, int[] indexes2)
+        {
+            int[,] newArr = new int[indexes1.Count(), indexes2.Count()];
+            for (int i = 0; i < indexes1.Count(); i++)
+            {
+                for (int j = 0; j < indexes2.Count(); j++)
+                {
+                    newArr[i, j] = arr[indexes1[i], indexes2[j]];
+                }
+            }
+
+            return newArr;
+        }
+
         public int[] FindMinimumsOfDistances(int[,] distances)
         {
             int[] minimums = new int[distances.GetLength(0)];
--- ImageProcessingDiplom/Program.cs
+++ ImageProcessingDiplom/Program.cs
@@ -81,11 +81,12 @@ Console.WriteLine("Matrix etalon time elapsed: " + stopwatch3.ElapsedMillisecond
 //    Console.WriteLine($"Mathes 1 image with {index} etalon: {item}");
 //    index++;
 //}
-var isSymmetric11 = _manhattan.MatrixIsSymmetric(distances11);
+////////////////////////////////////////////////////////////////////////////
+//var isSymmetric11 = _manhattan.MatrixIsSymmetric(distances11);
 
-string symetric = isSymmetric11 ? " " : "not ";
-results += "Matrix 1 is " + symetric + "symetric" + '\n';
-Console.WriteLine("Mathes 1 is " + symetric + "symetric");
+//string symetric = isSymmetric11 ? " " : "not ";
+//results += "Matrix 1 is " + symetric + "symetric" + '\n';
+//Console.WriteLine("Mathes 1 is " + symetric + "symetric");
 
 
 var mathes11 = _manhattan.CountThresholdMathes(distances11);
@@ -124,5 +125,89 @@ var mathes33 = _manhattan.CountThresholdMathes(distances33);
 results += "Mathes 3 with 3: " + mathes33 + '\n';
 Console.WriteLine("Mathes 3 with 3: " + mathes33);
 
+
+var v1 = _manhattan.FindMinimumV(distances11, distances12, distances13);
+var v2 = _manhattan.FindMinimumV(distances22 ,distances21, distances23);
+var v3 = _manhattan.FindMinimumV(distances33, distances31, distances32);
+
+Console.WriteLine();
+Console.WriteLine("V1 results:");
+foreach (var v in v1)
+{
+    if (v < -1) Console.Write(-v + " ");
+    else Console.Write(v + " ");
+}
+Console.WriteLine();
+Console.WriteLine("V2 results:");
+foreach (var v in v2)
+{
+    if (v < -1) Console.Write(-v + " ");
+    else Console.Write(v + " ");
+}
+Console.WriteLine();
+Console.WriteLine("V3 results:");
+foreach (var v in v3)
+{
+    if (v < -1) Console.Write(-v + " ");
+    else Console.Write(v + " ");
+}
+
+Console.WriteLine();
+var indexes1 = _manhattan.GetTopIndices(v1, 100);
+var indexes2 = _manhattan.GetTopIndices(v2, 100);
+var indexes3 = _manhattan.GetTopIndices(v3, 100);
+
+var newDistances11 = _manhattan.TakeTopElementsFromIndexes(distances11, indexes1);
+var newDistances12 = _manhattan.TakeTopElementsFrom2Indexes(distances12, indexes1,indexes2);
+var newDistances13 = _manhattan.TakeTopElementsFrom2Indexes(distances13, indexes1, indexes3);
+
+var newDistances21 = _manhattan.TakeTopElementsFrom2Indexes(distances21, indexes2, indexes1);
+var newDistances22 = _manhattan.TakeTopElementsFromIndexes(distances22, indexes2);
+var newDistances23 = _manhattan.TakeTopElementsFrom2Indexes(distances23, indexes2, indexes3);
+
+var newDistances31 = _manhattan.TakeTopElementsFrom2Indexes(distances31, indexes3, indexes1);
+var newDistances32 = _manhattan.TakeTopElementsFrom2Indexes(distances32, indexes3, indexes2);
+var newDistances33 = _manhattan.TakeTopElementsFromIndexes(distances33, indexes3);
+
+
+
+results += "For top 100 elements\n";
+Console.WriteLine("For top 100 elements:");
+
+var newmathes11 = _manhattan.CountThresholdMathes(newDistances11);
+results += "Mathes 1 with 1: " + newmathes11 + '\n';
+Console.WriteLine("Mathes 1 with 1: " + newmathes11);
+
+var newmathes12 = _manhattan.CountThresholdMathes(newDistances12);
+results += "Mathes 1 with 2: " + newmathes12 + '\n';
+Console.WriteLine("Mathes 1 with 2: " + newmathes12);
+
+var newmathes13 = _manhattan.CountThresholdMathes(newDistances13);
+results += "Mathes 1 with 3: " + newmathes13 + '\n';
+Console.WriteLine("Mathes 1 with 3: " + newmathes13);
+
+var newmathes21 = _manhattan.CountThresholdMathes(newDistances21);
+results += "Mathes 2 with 1: " + newmathes21 + '\n';
+Console.WriteLine("Mathes 2 with 1: " + newmathes21);
+
+var newmathes22 = _manhattan.CountThresholdMathes(newDistances22);
+results += "Mathes 2 with 2: " + newmathes22 + '\n';
+Console.WriteLine("Mathes 2 with 2: " + newmathes22);
+
+var newmathes23 = _manhattan.CountThresholdMathes(newDistances23);
+results += "Mathes 2 with 3: " + newmathes23 + '\n';
+Console.WriteLine("Mathes 2 with 3: " + newmathes23);
+
+var newmathes31 = _manhattan.CountThresholdMathes(newDistances31);
+results += "Mathes 3 with 1: " + newmathes31 + '\n';
+Console.WriteLine("Mathes 3 with 1: " + newmathes31);
+
+var newmathes32 = _manhattan.CountThresholdMathes(newDistances32);
+results += "Mathes 3 with 2: " + newmathes32 + '\n';
+Console.WriteLine("Mathes 3 with 2: " + newmathes32);
+
+var newmathes33 = _manhattan.CountThresholdMathes(newDistances33);
+results += "Mathes 3 with 3: " + newmathes33 + '\n';
+Console.WriteLine("Mathes 3 with 3: " + newmathes33);
 
 File.WriteAllText(projectPath + "\\results.txt", results);
